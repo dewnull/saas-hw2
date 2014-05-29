1557a74554db609dc0851e07329b490893319c69
Payman Touliat <payman@touliat.com>
2014-05-29 21:15:00 +0000

    fixed id for the links at table header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-  def sortable(column, title = nil)
+  def sortable(column, title = nil, link_class= nil)
   title ||= column.titleize
   direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
-  link_to title, :sort => column, :direction => direction
+    link_to title,{ :sort => column, :direction => direction}, {:id => link_class}
  end
 end
